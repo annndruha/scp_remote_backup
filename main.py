e5d48be5ab7b8098d1f79fe6726e2ee4cce50324
Andrey Marakulin <annndruha.github@gmail.com>
2024-04-17 11:49:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import shutil
 import json
 import datetime
 import dotenv
+import requests
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -25,48 +26,68 @@ with open('backup_folders.json') as f:
     backup_settings = json.load(f)
     # Settings asserts
 
+TEMP = 'temp'
+BACKUP_FOLDER = 'backups'
+
+
+def send_telegram_message(msg: object):
+    msg = str(msg)
+    if not len(msg):
+        return
+
+    logging.info(f'[Send message] {msg}')
+    r = requests.post(f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage'
+                      f'?parse_mode=HTML&chat_id={TG_CHAT_ID}&text={msg}',timeout=10)
+    if r.status_code != 200:
+        logging.error(f'[Telegram error] {r.status_code}, {r.text}')
+
 
 def catch_errors(func):
     def wrapper(*args, **kwargs):
         try:
             func(*args, **kwargs)
+        except subprocess.SubprocessError as err:
+            logging.error(err)
+            send_telegram_message('❌🔄 Backup failed: ' + str(err))
         except Exception as err:
             logging.error(err)
+            send_telegram_message('❌🔄 Backup failed: ' + str(err))
     return wrapper
 
 
-@catch_errors
 def execute(command):
     logging.info(command)
-    # try:
+    
     process = subprocess.Popen([*command.split()], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
     exit_code = process.wait()
     if exit_code != 0:
         raise subprocess.CalledProcessError(returncode=process.returncode, cmd=str(err))
-    # except subprocess.CalledProcessError as err:
-    #     logging.error(str(err))
 
 
+@catch_errors
 def make_backup(_archive):
-    if not os.path.exists('backups'):
-        os.makedirs('backups')
-    if not os.path.exists('temp'):
-        os.makedirs('temp')
+    send_telegram_message('▶️🔄 Starting backup: ' + _archive["name"])
+
+    if not os.path.exists(BACKUP_FOLDER):
+        os.makedirs(BACKUP_FOLDER)
+    if not os.path.exists(TEMP):
+        os.makedirs(TEMP)
 
     stamp = datetime.datetime.now().strftime("%Y_%d_%m_%H_%M_%S")
-
     name = f'{_archive["name"]}_{stamp}'
-    temp_folder = os.path.join('temp', name)
-
+    temp_folder = os.path.join(TEMP, name)
     os.makedirs(temp_folder)
 
     for remote_dir in _archive["remote_folders"]:
         cmd = f'scp -P {SSH_PORT} -i {SSH_PRIVATE_KEY_PATH} -r {SSH_USER}@{SSH_HOST}:{remote_dir} {temp_folder}'
         execute(cmd)
 
-    shutil.make_archive(f'{_archive["name"]}_{stamp}', 'zip', temp_folder)
+    archive_name = os.path.join(BACKUP_FOLDER, f'{_archive["name"]}_{stamp}')
+    shutil.make_archive(archive_name, 'zip', temp_folder)
     shutil.rmtree(temp_folder)
+
+    send_telegram_message('✅🔄 Backup successful: ' + _archive["name"])
 
 
 if __name__ == "__main__":
