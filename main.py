0bb393c7d4097f506739e4953f90868e76d7496d
Andrey Marakulin <annndruha.github@gmail.com>
2024-04-17 11:33:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
-import time
 import subprocess
-from dotenv import dotenv_values
 import logging
 import shutil
 import json
+import datetime
+import dotenv
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -12,39 +12,63 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-config = dotenv_values('.env')
+config = dotenv.dotenv_values('.env')
 SSH_HOST = config['SSH_HOST']
 SSH_USER = config['SSH_USER']
 SSH_PORT = config['SSH_PORT']
-SSH_PRIVATE_KEY_PATH = config['PRIVATE_KEY_PATH']
+SSH_PRIVATE_KEY_PATH = config['SSH_PRIVATE_KEY_PATH']
 
 TG_BOT_TOKEN = config['TG_BOT_TOKEN']
 TG_CHAT_ID = config['TG_CHAT_ID']
 
-
-def execute_remote(command):
-    logging.info(command)
-    try:
-        process = subprocess.Popen([*command.split()], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = process.communicate()
-        exit_code = process.wait()
-        if exit_code != 0:
-            raise subprocess.CalledProcessError(returncode=process.returncode, cmd=str(err))
-    except subprocess.CalledProcessError as err:
-        logging.error(str(err))
-
-
 with open('backup_folders.json') as f:
     backup_settings = json.load(f)
-
-for archive in backup_settings['backup_archives']:
-    temp_local_folder = f'temp_{archive["name"]}_{int(time.time())}'
-    remote_paths = archive["remote_folders"]
-
-    execute_remote(f'mkdir {os.path.join(os.getcwd(), temp_local_folder)}')
-    for remote_dir in remote_paths:
-        execute_remote(f'scp -P {SSH_PORT} -i {SSH_PRIVATE_KEY_PATH} -r {SSH_USER}@{SSH_HOST}:{remote_dir} {temp_local_folder}')
-    shutil.make_archive(f'{archive["name"]}_{int(time.time())}', 'zip', temp_local_folder)
-    shutil.rmtree(temp_local_folder)
+    # Settings asserts
 
 
+def catch_errors(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as err:
+            logging.error(err)
+    return wrapper
+
+
+@catch_errors
+def execute(command):
+    logging.info(command)
+    # try:
+    process = subprocess.Popen([*command.split()], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    exit_code = process.wait()
+    if exit_code != 0:
+        raise subprocess.CalledProcessError(returncode=process.returncode, cmd=str(err))
+    # except subprocess.CalledProcessError as err:
+    #     logging.error(str(err))
+
+
+def make_backup(_archive):
+    if not os.path.exists('backups'):
+        os.makedirs('backups')
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
+
+    stamp = datetime.datetime.now().strftime("%Y_%d_%m_%H_%M_%S")
+
+    name = f'{_archive["name"]}_{stamp}'
+    temp_folder = os.path.join('temp', name)
+
+    os.makedirs(temp_folder)
+
+    for remote_dir in _archive["remote_folders"]:
+        cmd = f'scp -P {SSH_PORT} -i {SSH_PRIVATE_KEY_PATH} -r {SSH_USER}@{SSH_HOST}:{remote_dir} {temp_folder}'
+        execute(cmd)
+
+    shutil.make_archive(f'{_archive["name"]}_{stamp}', 'zip', temp_folder)
+    shutil.rmtree(temp_folder)
+
+
+if __name__ == "__main__":
+    for archive in backup_settings['backup_archives']:
+        make_backup(archive)
